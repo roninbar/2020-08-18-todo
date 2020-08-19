32051cef54a685b672f10b2a290389eb7428614a
ron <rinbar70@gmail.com>
2020-08-19 16:14:55 +0300

    Connect <TodoForm> to Redux.

--- client/src/components/TodoForm.js
+++ client/src/components/TodoForm.js
@@ -4,6 +4,7 @@ import TextField from '@material-ui/core/TextField';
 import { MuiPickersUtilsProvider, DateTimePicker } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns'; // choose your lib
 import { Button } from '@material-ui/core';
+import { connect } from 'react-redux';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -14,16 +15,28 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-export default function () {
-    const [selectedDate, handleDateChange] = useState(new Date());
+export default connect(
+    null,
+    dispatch => ({ submit: ({ what, when }) => dispatch({ type: 'NEW', payload: { what, when } }) })
+)(function ({ submit }) {
+
+    const [what, setWhat] = useState('');
+    const [when, setWhen] = useState(new Date());
     const classes = useStyles();
+    const onSubmit = function (event) {
+        event.preventDefault();
+        submit({ what, when });
+    };
+    const onChangeWhat = function (event) {
+        return setWhat(event.target.value);
+    };
     return (
         <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <form className={classes.root} noValidate autoComplete="off">
-                <TextField id="filled-basic" label="What" variant="filled" />
-                <DateTimePicker value={selectedDate} onChange={handleDateChange} />
+            <form className={classes.root} noValidate autoComplete="off" onSubmit={onSubmit}>
+                <TextField id="filled-basic" label="What" variant="filled" value={what} onChange={onChangeWhat} />
+                <DateTimePicker value={when} onChange={setWhen} />
                 <Button type="submit" variant="contained" color="primary">Add</Button>
             </form>
         </MuiPickersUtilsProvider>
     );
-}
+});
