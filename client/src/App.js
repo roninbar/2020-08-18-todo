2fd385e0af9ce2ffe14e5e45899fa35b022806fe
ron <rinbar70@gmail.com>
2020-08-19 13:36:08 +0300

    Basic Redux setup.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { createStore } from 'redux';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
+import reducers from './reducers';
+
+const store = createStore(reducers);
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/signin">
+      <Provider store={store}>
+        <Router>
+          <Switch>
+            <Route path="/signin">
 
-          </Route>
-          <Route path="/signup">
+            </Route>
+            <Route path="/signup">
 
-          </Route>
-          <Route path="/">
-            <TodoForm />
-            <TodoList />
-          </Route>
-        </Switch>
-      </Router>
+            </Route>
+            <Route path="/">
+              <TodoForm />
+              <TodoList />
+            </Route>
+          </Switch>
+        </Router>
+      </Provider>
     </div>
   );
 }
