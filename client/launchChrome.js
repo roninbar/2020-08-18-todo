29bd54e0773b509b355c3de66bd82b0ad4fd7311
ron <rinbar70@gmail.com>
2020-08-21 23:58:41 +0300

    Don't disable Chrome extensions in launched instance.

--- client/launchChrome.js
+++ client/launchChrome.js
@@ -1,8 +1,10 @@
 const chromeLauncher = require('chrome-launcher');
-console.info(process.argv);
+
 chromeLauncher.launch({
-    startingUrl: process.argv[2],
     port: 9222,
+    ignoreDefaultFlags: true,
+    chromeFlags: chromeLauncher.Launcher.defaultFlags().filter(flag => flag !== '--disable-extensions'),
+    startingUrl: process.argv[process.argv.length - 1],
 }).then(function (chrome) {
     console.info('Chrome remote debugging port:', chrome.port);
 });
