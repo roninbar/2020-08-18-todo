992f71fcbbeb3f0bc52a0d253478aaff4608917d
ron <rinbar70@gmail.com>
2020-08-21 13:31:18 +0300

    Use Roboto font.

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -7,6 +7,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import 'fontsource-roboto';
 import React from 'react';
 import { connect } from 'react-redux';
 
