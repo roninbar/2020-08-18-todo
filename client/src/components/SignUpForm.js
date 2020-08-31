7b87e89cfa1992daf90e35dc711e253a0be7690c
ron <rinbar70@gmail.com>
2020-08-31 23:41:08 +0300

    Change <SignUpForm> from a function to a class and implement Ajax functionality.

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { TextField, withStyles, Button } from '@material-ui/core';
 
 const styles = theme => ({
@@ -12,26 +12,72 @@ const styles = theme => ({
     },
 });
 
-function SignUpForm({ classes }) {
-    return (
-        <form className={classes.root} noValidate autoComplete="off" onSubmit={e => e.preventDefault()}>
-            <TextField
-                type="text"
-                variant="outlined"
-                label="Username" />
-            <TextField
-                type="password"
-                variant="outlined"
-                label="Password"
-            />
-            <TextField
-                type="password"
-                variant="outlined"
-                label="Repeat Password"
-            />
-            <Button type="submit" variant="contained" color="primary" size="large">Sign Up</Button>
-        </form>
-    );
+class SignUpForm extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            password2: '',
+        };
+    }
+
+    onChange({ target: { name, value } }) {
+        this.setState({ [name]: value });
+    }
+
+    async onSubmit(e) {
+        e.preventDefault();
+        const { username, password, password2 } = this.state;
+        if (username && password === password2) {
+            const body = new FormData();
+            body.set('username', username);
+            body.set('password', password);
+            const response = await fetch('/signup', {
+                method: 'POST',
+                body,
+            });
+        }
+    }
+
+    render() {
+        const { username, password, password2 } = this.state;
+        const { classes } = this.props;
+        return (
+            <form className={classes.root} noValidate autoComplete="off" onSubmit={this.onSubmit.bind(this)}>
+                <TextField
+                    variant="outlined"
+                    type="text"
+                    name="username"
+                    label="Username"
+                    error={!username}
+                    value={username}
+                    onChange={this.onChange.bind(this)}
+                />
+                <TextField
+                    variant="outlined"
+                    type="password"
+                    name="password"
+                    label="Password"
+                    error={password !== password2}
+                    value={password}
+                    onChange={this.onChange.bind(this)}
+                />
+                <TextField
+                    variant="outlined"
+                    type="password"
+                    name="password2"
+                    label="Repeat Password"
+                    error={password !== password2}
+                    value={password2}
+                    onChange={this.onChange.bind(this)}
+                />
+                <Button variant="contained" type="submit" color="primary" size="large">Sign Up</Button>
+            </form>
+        );
+    }
+
 }
 
 export default withStyles(styles)(SignUpForm);
