429065a7259e27ad635ea0773a98876f74081302
ron <rinbar70@gmail.com>
2020-09-05 18:24:58 +0300

    Factor out passport setup to ./util/passport.

--- server/app.js
+++ server/app.js
@@ -3,33 +3,7 @@ var session = require('express-session');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var passport = require('passport');
-var passportLocal = require('passport-local');
-var mysql = require('mysql2/promise');
-const { hash } = require('./util/hash');
-
-passport.use(new passportLocal.Strategy(async function (username, password, done) {
-    const conn = await mysql.createConnection({
-        user: 'root',
-        database: 'todo',
-    });
-    try {
-        const [[user]] = await conn.execute('SELECT `id` FROM `user` WHERE `name` = ? AND `password_hash` = ?', [username, hash(password)]);
-        return done(null, user, { message: user ? 'Greetings, Professor Falken!' : 'Invalid username/password.' });
-    } catch (error) {
-        return done(error);
-    } finally {
-        await conn.end();
-    }
-}));
-
-passport.serializeUser(function ({ id }, done) {
-    done(null, id);
-})
-
-passport.deserializeUser(function (id, done) {
-    done(null, id);
-})
+var passport = require('./util/passport');
 
 var app = express();
 
@@ -42,12 +16,12 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(function (req, res, next) {
     console.info('User:', req.user);
     next();
 });
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.post('/login',
     passport.authenticate('local', { session: true }),
@@ -55,6 +29,7 @@ app.post('/login',
         res.sendStatus(200);
     }
 );
+
 app.post('/logout', function (req, res) { req.logOut(); res.sendStatus(204); })
 
 app.use('/user', require('./routes/users'));
